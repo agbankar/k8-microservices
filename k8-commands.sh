e1823710b9a9ad2f1066f26a3fdd97d36cc54bcb
AS008BA <ashish.bankar@pb.com>
2021-07-04 12:40:59 +0530

    working solution with deployment

---
+++ k8-commands.sh
@@ -0,0 +1,2 @@
+kubectl delete pod user-service-p
+kubectl exec -it  user-service-p -- bash
---
+++ user-service/src/main/java/com/UserController.java
@@ -0,0 +1,18 @@
+package com;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping("/getUser/{id}")
+    public ResponseEntity<User> getUser(@PathVariable String id) {
+        User ashish = new User(1, "ashish");
+        return ResponseEntity.ok(ashish);
+    }
+
+}
