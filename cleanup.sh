b07098ae9a0f468b52f1ca2c482b105e700399f7
agbankar <ashishbankar@Ashishs-MacBook-Pro.local>
2021-11-05 14:04:13 +0100

    working till 12.0.0.1

--- cleanup.sh
+++ cleanup.sh
@@ -1,11 +1,5 @@
-kubectl delete svc authentication-service-s order-service-s user-service-s
-kubectl delete deployment authentication-service order-service user-service
-docker rmi authentication-service user-service order-service
-mvn clean install -DskipTests
-cd authentication-service  && docker build -t authentication-service . && cd ..
-cd order-service  && docker build -t order-service . && cd ..
-cd user-service  && docker build -t user-service . && cd ..
-kubectl apply -f definition.yml
-docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
-
-
+kubectl delete svc  authentication-service-s order-service-s user-service-s
+#sleep 10s
+kubectl delete deployment order-service user-service authentication-service
+docker rmi  user-service:latest order-service:latest authentication-service
+#docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
--- initcluster.sh
+++ initcluster.sh
@@ -1,2 +1,8 @@
-#kubectl delete pod user-service-p
+mvn clean install -DskipTests
+cd order-service  && docker build -t order-service . && cd ..
+cd user-service  && docker build -t user-service . && cd ..
+cd authentication-service  && docker build -t authentication-service . && cd ..
 kubectl apply -f definition.yml
+docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
+
+
